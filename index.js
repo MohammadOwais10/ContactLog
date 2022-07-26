d0d68c79a7af53ecb153ab27447b81264d342f9f
MohammadOwais10 <owais0945@gmail.com>
2022-07-26 21:16:33 +0530

    Start Server and Render Page

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const path = require("path");
+const port = 8000;
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+var contactLog = [
+  {
+    name: "Owais",
+    phone: "9876543210",
+  },
+  {
+    name: "CodingNinjas",
+    phone: "18001233598",
+  },
+  {
+    name: "Alvi",
+    phone: "8833551100",
+  },
+];
+
+app.get("/", (request, response) => {
+  console.log(request.url);
+  return response.render("home", { title: "render" });
+});
+
+app.listen(port, (error) => {
+  if (error) {
+    console.log("There was some problem in starting the server!");
+    return;
+  } else {
+    console.log("server is running on the port", port);
+  }
+});
