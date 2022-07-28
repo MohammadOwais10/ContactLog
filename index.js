01454f5a35387600e8fe505f5dac7571bced9e10
MohammadOwais10 <owais0945@gmail.com>
2022-07-29 00:12:12 +0530

    Add Background

--- index.js
+++ index.js
@@ -5,39 +5,34 @@ const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("assets"));
 
-var contactLog = [
-  {
-    name: "Owais",
-    phone: "9876543210",
-  },
-  {
-    name: "CodingNinjas",
-    phone: "18001233598",
-  },
-  {
-    name: "Alvi",
-    phone: "8833551100",
-  },
-];
+var contactLog = [];
+// {
+//   name: "Owais",
+//   phone: "9876543210",
+// },
+// {
+//   name: "CodingNinjas",
+//   phone: "18001233598",
+// },
+// {
+//   name: "Alvi",
+//   phone: "8833551100",
+// },
 
-app.get("/", (request, response) => {
-  console.log(request.url);
-  return response.render("home", {
-    title: "render",
+app.get("/", (req, res) => {
+  var options = {
+    title: "Contact Log",
     contact_log: contactLog,
-  });
+  };
+  return res.render("home", options);
 });
 
-app.get("/addContact", (req, res) => {
-  return res.render("addContact", {});
-});
-
-app.post("/create-contact", (request, response) => {
-  //return response.redirect("/addContact");
-  contactLog.push({ name: request.body.name, phone: request.body.phone });
-  return response.redirect("back");
+app.post("/create-contact", (req, res) => {
+  contactLog.push(req.body);
+  return res.redirect("back");
 });
 
 app.listen(port, (error) => {
